958dd5318490bc97a25ff85ad1232618497b47bb
parzevaI <66918776+parzevaI@users.noreply.github.com>
2023-10-14 20:54:16 -0400

    Added regenerate comment in generator file

--- generator.py
+++ generator.py
@@ -2,12 +2,14 @@ from faker import Faker
 from tabulate import tabulate
 import random as rm
 
+# NEED TO REGENERATE THE SEARCHES FILE SO IT CONTAINS A WHOLE BUNCH OF THE EMAIL 
+# PROVIDER SITE SO THE PLAYER HAS TO CHECK THE TIME IT WAS SENT
 
 fake = Faker()
 
 # Generate random search history data
 ip_addresses = [fake.ipv4() for _ in range(200)]
-urls = [fake.url() for _ in range(200)]
+urls = [fake.url() for _ in range(200)] # + ["https://google.com/mail"]
 
 # List it
 searches = [(
